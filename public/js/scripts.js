d65f502e77956c43b1b135cfa98ff77e0d5464f9
Minh-Tuan <68106241+MTDance@users.noreply.github.com>
2020-10-15 20:07:50 +0200

    1er commit de l'univers sur mon projet

---
+++ public/js/scripts.js
@@ -0,0 +1,20 @@
+// Création de la barre de progresssion sur js
+window.onload = () => {
+    // Ecouteur d'évenement sur scroll
+    window.addEventListener("scroll", () => {
+        //Calcul de la hauteur "utile" du document
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight
+
+        //Récupération de la postion verticale
+        let position = window.scrollY
+
+        //Récupération de la largur de la fenetre
+        let largeur = document.documentElement.clientWidth
+
+        // Calcul de la largeur de la barre
+        let barre = position / hauteur * largeur
+
+        // Modification du CSS de la barre
+        document.getElementById("progress").style.width = barre+"px"
+    })
+}
