55185bf8ccd348f465e7142fd9048614150907a4
Minh-Tuan <68106241+MTDance@users.noreply.github.com>
2020-10-19 13:41:03 +0200

    liens sur les cours ok

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -16,4 +16,95 @@ class CoursController extends AbstractController
             'controller_name' => 'CoursController',
         ]);
     }
+
+    /**
+     * @Route("/eveil", name="eveil")
+     */
+    public function eveil()
+    {
+        return $this->render('cours/eveil.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/classique", name="classique")
+     */
+    public function classique()
+    {
+        return $this->render('cours/classique.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/contemporain", name="contemporain")
+     */
+    public function contemporain()
+    {
+        return $this->render('cours/contemporain.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/modern", name="modern")
+     */
+    public function modern()
+    {
+        return $this->render('cours/modern.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/ftp", name="ftp")
+     */
+    public function ftp()
+    {
+        return $this->render('cours/ftp.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/yoga", name="yoga")
+     */
+    public function yoga()
+    {
+        return $this->render('cours/yoga.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/ragga", name="ragga")
+     */
+    public function ragga()
+    {
+        return $this->render('cours/ragga.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/tbc", name="tbc")
+     */
+    public function tbc()
+    {
+        return $this->render('cours/tbc.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
+    /**
+     * @Route("/hip_hop", name="hip_hop")
+     */
+    public function hip_hop()
+    {
+        return $this->render('cours/hip_hop.html.twig', [
+            'controller_name' => 'CoursController',
+        ]);
+    }
+
 }
