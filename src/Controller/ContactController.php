0c121db640ead598fc7da0741048741d6fc5e91e
Minh-Tuan <68106241+MTDance@users.noreply.github.com>
2020-12-11 23:49:41 +0100

    tarif cours à l'année ok

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -47,6 +47,22 @@ class ContactController extends AbstractController
         ]);
     }
 
+       /**
+     *
+     * 
+     * 
+     * A partir de là ce sont les vues des 4 cours de danse
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+
     /**
      * @Route("/eveil_payment", name="eveil_payment")
      */
@@ -74,6 +90,14 @@ class ContactController extends AbstractController
     }
 
     /**
+     * @Route("/1cours_payment_adulte", name="1courspayment_adulte")
+     */
+    public function cours_payment_adulte()
+    {
+        return $this->render('contact/1courspayment_adulte.html.twig');
+    }
+
+    /**
      * @Route("/2cours_payment", name="2courspayment")
      */
     public function deux_cours_payment()
@@ -98,21 +122,27 @@ class ContactController extends AbstractController
     }
 
     /**
-     * @Route("/1cours_payment_adulte", name="1courspayment_adulte")
-     */
-    public function cours_payment_adulte()
-    {
-        return $this->render('contact/1courspayment_adulte.html.twig');
-    }
-
-    
-
-    /**
      * @Route("/3cours_payment", name="3courspayment")
      */
     public function trois_cours_payment()
     {
         return $this->render('contact/3courspayment.html.twig');
+    }
+
+    /**
+     * @Route("/3cours_payment_enfant", name="3courspayment_enfant")
+     */
+    public function trois_cours_payment_enfant()
+    {
+        return $this->render('contact/3courspayment_enfant.html.twig');
+    }
+
+    /**
+     * @Route("/3cours_payment_adulte", name="3courspayment_adulte")
+     */
+    public function trois_cours_payment_adulte()
+    {
+        return $this->render('contact/3courspayment_adulte.html.twig');
     }
 
     /**
@@ -124,6 +154,37 @@ class ContactController extends AbstractController
     }
 
     /**
+     * @Route("/4cours_payment_enfant", name="4courspayment_enfant")
+     */
+    public function quatre_cours_payment_enfant()
+    {
+        return $this->render('contact/4courspayment_enfant.html.twig');
+    }
+
+    /**
+     * @Route("/4cours_payment_adulte", name="4courspayment_adulte")
+     */
+    public function quatre_cours_payment_adulte()
+    {
+        return $this->render('contact/4courspayment_adulte.html.twig');
+    }
+
+    /**
+     *
+     * 
+     * 
+     * A partir de là ce sont les tarifs des 4 cours de danse à l'année
+     * avec l'utilisation stripe
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+    /**
      * Tarif cours Eveil
      * @Route("/create-checkout-session", name="checkout")
      */
@@ -132,8 +193,10 @@ class ContactController extends AbstractController
         // installation de la clé privée
         \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
 
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
             'line_items' => [[
               'price_data' => [
                 'currency' => 'eur',
@@ -145,6 +208,8 @@ class ContactController extends AbstractController
               'quantity' => 1,
             ]],
             'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
             'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
           ]);
@@ -160,8 +225,10 @@ class ContactController extends AbstractController
         // installation de la clé privée
         \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
 
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
             'line_items' => [[
               'price_data' => [
                 'currency' => 'eur',
@@ -173,6 +240,8 @@ class ContactController extends AbstractController
               'quantity' => 1,
             ]],
             'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
             'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
           ]);
@@ -188,8 +257,10 @@ class ContactController extends AbstractController
         // installation de la clé privée
         \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
 
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
             'line_items' => [[
               'price_data' => [
                 'currency' => 'eur',
@@ -201,6 +272,72 @@ class ContactController extends AbstractController
               'quantity' => 1,
             ]],
             'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
+            'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+          ]);
+
+        return new JsonResponse([ 'id' => $session->id ]);
+    }
+
+    /**
+     * @Route("/3cours_enfant", name="3cours_enfant")
+     */
+    public function trois_cours_enfant()
+    {
+        // installation de la clé privée
+        \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
+
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => 'Tarif 3 cours enfant/étudiant Rythme Danse',
+                ],
+                'unit_amount' => 61000,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
+            'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+          ]);
+
+        return new JsonResponse([ 'id' => $session->id ]);
+    }
+
+    /**
+     * @Route("/4cours_enfant", name="4cours_enfant")
+     */
+    public function quatre_cours_enfant()
+    {
+        // installation de la clé privée
+        \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
+
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => 'Tarif 4 cours enfant/étudiant Rythme Danse',
+                ],
+                'unit_amount' => 72500,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
             'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
           ]);
@@ -216,8 +353,10 @@ class ContactController extends AbstractController
         // installation de la clé privée
         \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
 
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
             'line_items' => [[
               'price_data' => [
                 'currency' => 'eur',
@@ -229,6 +368,8 @@ class ContactController extends AbstractController
               'quantity' => 1,
             ]],
             'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
             'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
           ]);
@@ -244,8 +385,10 @@ class ContactController extends AbstractController
         // installation de la clé privée
         \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
 
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
             'line_items' => [[
               'price_data' => [
                 'currency' => 'eur',
@@ -257,6 +400,72 @@ class ContactController extends AbstractController
               'quantity' => 1,
             ]],
             'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
+            'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+          ]);
+
+        return new JsonResponse([ 'id' => $session->id ]);
+    }
+
+    /**
+     * @Route("/3cours_adulte", name="3cours_adulte")
+     */
+    public function trois_cours_adulte()
+    {
+        // installation de la clé privée
+        \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
+
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => 'Tarif 3 cours adulte Rythme Danse',
+                ],
+                'unit_amount' => 73500,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
+            'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+          ]);
+
+        return new JsonResponse([ 'id' => $session->id ]);
+    }
+
+    /**
+     * @Route("/4cours_adulte", name="4cours_adulte")
+     */
+    public function quatre_cours_adulte()
+    {
+        // installation de la clé privée
+        \Stripe\Stripe::setApiKey('sk_test_51HtCJ1JN4HFVflDj73HeHqZAry5HfOT6cD5ZuRkRHi2GaAjmhU6sqbQofir3nDKbTR44drcjaBtYJ9QOxK69omti00DgkmSOly');
+
+        // ce bout de code veut savor ce qu'on veut dans notre paiement
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            // Tout ce qui concerne le produit
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => 'Tarif 4 cours adulte Rythme Danse',
+                ],
+                'unit_amount' => 94500,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            // création de lien vers url en cas de succes ou en cas d'echecs
+            // url GeneratorInterface est une url absolu
             'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
           ]);
